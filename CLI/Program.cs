2816c9c1eeeca03c5d3b8cacbbd98533cfd2e68c
ABSchmidt <32262030+ABSchmidt@users.noreply.github.com>
2024-10-22 20:15:00 +0200

    sln file rename

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -8,6 +8,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        foreach(string arg in args)
+        {
+            Console.WriteLine(arg);
+        }
         ApplicationService.FlattenJSON("path-to-source", "path-to-output");
         Console.ReadKey();
     }

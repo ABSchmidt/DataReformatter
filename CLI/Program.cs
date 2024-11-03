07d91a523d8a1c9c8ca80328909768cf289b66e3
Anders Bastian Christiansen Schmidt <anders.schmidt@twoday.com>
2024-11-03 18:13:14 +0100

    json to csv

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -17,11 +17,13 @@ class Program
         // string flattenedOutputPath = "D:\\Programming\\Repositories\\DataReformatter\\Assets\\Output\\flattened.json";
         // ApplicationService.FlattenJSON(nestedSourcePath, flattenedOutputPath);
         
-        string flatSourcePath = "D:\\Programming\\Repositories\\DataReformatter\\Assets\\Source\\flat.json";
-        string unflattenedOutputPath = "D:\\Programming\\Repositories\\DataReformatter\\Assets\\Output\\unflattened.json";
-        ApplicationService.UnflattenJSON(flatSourcePath, unflattenedOutputPath);
+        // string flatSourcePath = "D:\\Programming\\Repositories\\DataReformatter\\Assets\\Source\\flat.json";
+        // string unflattenedOutputPath = "D:\\Programming\\Repositories\\DataReformatter\\Assets\\Output\\unflattened.json";
+        // ApplicationService.UnflattenJSON(flatSourcePath, unflattenedOutputPath);
         
-        
+        string nestedSourcePath = "C:\\Users\\AndersBastianChristi\\Privat\\Repos\\DataReformatter\\Assets\\Source\\nested.json";
+        string csvOutputPath = "C:\\Users\\AndersBastianChristi\\Privat\\Repos\\DataReformatter\\Assets\\Output\\result.csv";
+        ApplicationService.JsonToCsv(nestedSourcePath, csvOutputPath);
         //Console.ReadKey();
     }
 }
